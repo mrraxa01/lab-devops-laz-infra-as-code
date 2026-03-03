547e9b52af6da2b86293313ecdf291258362fb47
Marcio Rodrigues <mrraxa01@gmail.com>
2026-03-03 01:45:49 -0300

    add install docker in ec2

--- ec2.tf
+++ ec2.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "lab2-laz_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.lab2-laz_sg.id
-  cidr_ipv4         = "201.71.38.149/32"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
